da45bdc310d74dd1b890728303617438e50781db
juhyeontae <jabel1733@gmail.com>
2024-06-06 16:28:44 +0900

    feat: 컨트롤러부분 코드 작성 및 테스트 작성
    컨트롤러부분 코드 작성 및 테스트 작성

---
+++ src/test/java/com/musinsa/api/controller/ProductControllerTest.java
@@ -0,0 +1,43 @@
+package com.musinsa.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musinsa.api.dto.ProductAddReq;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("상품 추가")
+    void addProductTest() throws Exception {
+        ProductAddReq productAddReq = new ProductAddReq();
+        productAddReq.setCategoryId(1l);
+        productAddReq.setBrandId(3l);
+        productAddReq.setPrice(300);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(productAddReq);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("productId")));
+    }
+}
